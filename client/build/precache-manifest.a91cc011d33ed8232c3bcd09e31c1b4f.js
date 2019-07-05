fdcdedb58b602d04bd815ae4d4934ae33d618dab
Lapkovskiy <Yevgeniy.Lapkovskiy@billing.ru>
2019-07-05 11:38:38 +0300

    second commit

---
+++ client/build/precache-manifest.a91cc011d33ed8232c3bcd09e31c1b4f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7802d921364ebf285705e13f4e317e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ace5eaa538efe1b2c27",
+    "url": "/static/js/2.b247fe0b.chunk.js"
+  },
+  {
+    "revision": "20f82a47b1646eb4234f",
+    "url": "/static/js/main.fbe7ee1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
