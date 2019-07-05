ca3e2a031d070ba64b125186f671c23b26eb0599
Lapkovskiy <Yevgeniy.Lapkovskiy@billing.ru>
2019-07-05 11:40:21 +0300

    Updates

---
+++ precache-manifest.d06b574a6e8d502835820ff267293c64.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280677e4b9dec41bffb80f159f49482b",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "0ace5eaa538efe1b2c27",
+    "url": "/gallery/static/js/2.b247fe0b.chunk.js"
+  },
+  {
+    "revision": "20f82a47b1646eb4234f",
+    "url": "/gallery/static/js/main.fbe7ee1d.chunk.js"
+  },
+  {
+    "revision": "de9a95ae35c4a918bf4e",
+    "url": "/gallery/static/js/runtime~main.3e4d5bb6.js"
+  }
+]);
